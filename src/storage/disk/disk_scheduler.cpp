59783d5116e1875cb8a4a80fcbee4426681749ce
Ziyi Chen <zy035c@gmail.com>
2024-01-25 23:58:27 +0800

    passed disk scheduler testgit add .

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -18,9 +18,9 @@ namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
   // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
+  // throw NotImplementedException(
+  //     "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
+  //     "throw exception line in `disk_scheduler.cpp`.");
 
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
@@ -34,8 +34,38 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  this->request_queue_.Put(std::move(r));
+}
 
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  while (true) {
+    if (this->disk_manager_ == nullptr) {
+      return;
+    }
+
+    
+    auto req_opt = this->request_queue_.Get();
+    if (req_opt == std::nullopt) {
+      return;
+    }
+    auto req = std::move(req_opt.value());
+    if (req.is_write_) {
+      this->disk_manager_->WritePage(
+        req.page_id_,
+        req.data_
+      );
+    } else {
+      this->disk_manager_->ReadPage(
+        req.page_id_,
+        req.data_
+      );
+    }
+
+    // Remember to set the value on the DiskRequest's callback to
+    // signal to the request issuer that the request has been completed.
+    req.callback_.set_value(true);
+  }
+}
 
 }  // namespace bustub
