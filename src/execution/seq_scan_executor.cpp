5c5fcbe07b9171ecabb4da91c06bb50f71d85e6f
Ziyi Chen <zy035c@gmail.com>
2024-02-06 19:20:41 -0500

    seq_scan_ok

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -14,10 +14,29 @@
 
 namespace bustub {
 
-SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx) {}
+SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNode *plan) : AbstractExecutor(exec_ctx), plan_(plan) {}
 
-void SeqScanExecutor::Init() { throw NotImplementedException("SeqScanExecutor is not implemented"); }
+void SeqScanExecutor::Init() {
+    auto oid = this->plan_->GetTableOid();
+    auto table = this->GetExecutorContext()->GetCatalog()->GetTable(oid);
+    this->it = &table->table_->MakeIterator();
+}
 
-auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+    if (it->IsEnd()) {
+        return false;
+    }
+    auto pair = it->GetTuple();
+    auto rid_ = it->GetRID();
+    if (pair.first.is_deleted_) {
+        ++(*it);  // ? +it
+        return this->Next(tuple, rid);
+    }
+
+    *tuple = pair.second;
+    *rid = rid_;
+    ++(*it);
+    return true;
+}
 
 }  // namespace bustub
