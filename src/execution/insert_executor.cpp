ee67c65af9188a8e0f46727c42e431be3c9aa700
Ziyi Chen <zy035c@gmail.com>
2024-02-06 23:08:40 -0500

    work on more executor

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -18,10 +18,59 @@ namespace bustub {
 
 InsertExecutor::InsertExecutor(ExecutorContext *exec_ctx, const InsertPlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void InsertExecutor::Init() { throw NotImplementedException("InsertExecutor is not implemented"); }
+void InsertExecutor::Init() { 
+    // TODO
+}
 
-auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool { return false; }
+auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
+    auto oid = this->plan_->GetTableOid();
+    auto catalog = this->GetExecutorContext()->GetCatalog();
+    auto table = catalog->GetTable(oid); 
+    auto idxs = catalog->GetTableIndexes(table->name_);
+
+    int32_t count = 0;
+    while (1) {
+        Tuple tuple_;
+        auto ok = this->child_executor_->Next(&tuple_, rid);
+        if (!ok) {
+            break;
+        }
+
+        auto rid__ = table->table_->InsertTuple({0, false}, tuple_);
+        if (rid__ == std::nullopt) {
+            throw bustub::Exception("insertion failed");
+            return false;
+        }
+
+        for (auto index : idxs) {
+            index->index_->InsertEntry(
+                tuple_.KeyFromTuple(
+                    table->schema_,
+                    index->key_schema_,
+                    index->index_->GetKeyAttrs()
+                ),
+                rid__.value(),
+                &Transaction(INVALID_TXN_ID)
+                // For the insertion_txn_ and the deletion_txn_ fields, 
+                // just set it to INVALID_TXN_ID. 
+                // These fields are intended to be used in future 
+                // semesters where we might switch to an MVCC storage.
+            );
+        }
+
+        ++count;
+    }
+    if (count == 0) {
+        return false;
+    }
+
+    *tuple = Tuple{
+        std::vector<Value>{{TypeId::INTEGER, count}}, 
+        &this->GetOutputSchema()
+    };
+    return true; 
+}
 
 }  // namespace bustub
